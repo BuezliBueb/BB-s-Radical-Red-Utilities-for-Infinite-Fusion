23ca26b7e77d369bb06cd95787b02c985f5e2db0
BuezliBueb <164735539+BuezliBueb@users.noreply.github.com>
2024-03-24 11:10:15 +0100

    Fixing Crash when not saved

--- Data/Scripts/998_Mods/008_BB_RR_Utilties/002_BB_Pokevial/001_BB_RR_Pokevial.rb
+++ Data/Scripts/998_Mods/008_BB_RR_Utilties/002_BB_Pokevial/001_BB_RR_Pokevial.rb
@@ -11,7 +11,7 @@ RRMenuCommands.register("healPokemon",{
 class Interpreter
   def command_314
     $Trainer.heal_party if @parameters[0] == 0
-    file = File.open("Data/Scripts/998_mods/008_BB_RR_Utilties/002_BB_Pokevial/" + $Trainer.save_slot + ".txt", "w")
+    file = File.open("Data/Scripts/998_mods/008_BB_RR_Utilties/002_BB_Pokevial/" + ($Trainer.save_slot == nil ? "Unsaved" : $Trainer.save_slot)  + ".txt", "w")
     file.write(0)
     file.close
     return true
